a643f068964afa0ea4051e23854599283c1031d7
Muhammad Muazzain <90068510+MuhammadMuazzain@users.noreply.github.com>
2025-09-18 21:15:55 +0500

    Final update

--- version_old/data_utils.py
+++ version_old/data_utils.py
@@ -72,11 +72,3 @@ class DataSet():
     def transform(self, img):
 
         return img
-    
-    def transform(self, img):
-
-        return img
-
-    def transform(self, img):
-
-        return img
